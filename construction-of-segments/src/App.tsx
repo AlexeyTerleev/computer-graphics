dfce1078da78ca082cc6e47ef905b9feeeec0670
alexey_t <alexterleev682004@gmail.com>
2024-02-20 21:45:58 +0300

    done

--- construction-of-segments/src/App.tsx
+++ construction-of-segments/src/App.tsx
@@ -1,25 +1,66 @@
 import React, { useState } from 'react';
+import Point, {Color} from './pointInterface';
+import {digitalDifferentialAnalyzer, bresenhamAlgorithm, xiaolinWuAlgorithm} from "./algs"
+
 import './App.css';
 
-interface Point {
-  x: number;
-  y: number;
-}
 
 const App: React.FC = () => {
+  const [segmentEnds, setSegmentEnds] = useState<Point[]>([]);
   const [points, setPoints] = useState<Point[]>([]);
+  const [color, setColor] = useState<Color>({r: 255, g: 0, b: 0});
+
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState("digitalDifferentialAnalyzer");
   const [cellSize, setCellSize] = useState(25);
-  const [showGrid, setShowGrid] = useState(true);
+  const [showGrid, setShowGrid] = useState(false);
+  const [debug, setDebug] = useState(false);
+  const [debugRunning, setDebugRunning] = useState(false);
+  const [counter, setCouter] = useState(0);
+  
   const width = 500;
   const height = 500;
+
+  const handleAlgorithmChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSelectedAlgorithm(selectedValue);
+  };
+
+  const handleRun = () => {
+    setCouter(0);
+    setDebugRunning(debug);
+
+    if (selectedAlgorithm === "digitalDifferentialAnalyzer") {
+      setPoints(digitalDifferentialAnalyzer(segmentEnds, color));
+    }
+    else if (selectedAlgorithm === "bresenhamAlgorithm") {
+      setPoints(bresenhamAlgorithm(segmentEnds, color));
+    }
+    else if (selectedAlgorithm === "xiaolinWuAlgorithm") {
+      setPoints(xiaolinWuAlgorithm(segmentEnds, color));
+    }
+  }
+
+  const handleStep = () => {
+    if (counter === points.length) {
+      setDebugRunning(false);
+      return;
+    }
+    setCouter(counter + 1);
+  }
+
+  const handleReset = () => {
+    setCouter(0);
+    setPoints([]);
+    setSegmentEnds([]);
+    setDebugRunning(false);
+  }
 
   const toggleGrid = () => {
     if (!showGrid && cellSize < 10) {
       alert("Grid only for cell size > 10");
+      return;
     }
-    else {
-      setShowGrid(!showGrid);
-    }
+    setShowGrid(!showGrid);
   }
 
   const handleCellSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,75 +81,19 @@ const App: React.FC = () => {
 
     const point: Point = {
       x: Math.floor(cursorPt.x / cellSize), 
-      y: Math.floor(cursorPt.y / cellSize)
+      y: Math.floor(cursorPt.y / cellSize),
+      opacity: 1,
+      color: {r: 0, b: 0, g: 255},
     }
 
-    if (points.length >= 2) {
-      setPoints([point]);
+    if (segmentEnds.length === 2) {
+      handleReset();
+      setSegmentEnds([point]);
     } else {
-      setPoints([...points, point]);
+      setSegmentEnds([...segmentEnds, point]);
     }
   };
-
-  const digitalDifferentialAnalyzer = () => {
-    if (points.length != 2) return;
-
-    const start = points[0];
-    const end = points[1];
-
-    const length = Math.max(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
-
-    const deltaX = -(start.x - end.x) / length;
-    const deltaY = -(start.y - end.y) / length;
-
-    const newPoints = [start];
-    for (let i = 1; i < length; i++) {
-      const currentPoint: Point = {
-        x: Math.round(start.x + i * deltaX),
-        y: Math.round(start.y + i * deltaY),
-      };
-      newPoints.push(currentPoint);
-    }
-    newPoints.push(end);
-    setPoints(newPoints);
-  };
-
-  const bresenhamAlgorithm = () => {
-    if (points.length !== 2) return;
   
-    const start = points[0];
-    const end = points[1];
-  
-    let currentX = start.x;
-    let currentY = start.y;
-  
-    const deltaX = Math.abs(end.x - start.x);
-    const deltaY = Math.abs(end.y - start.y);
-  
-    const stepX = start.x < end.x ? 1 : -1;
-    const stepY = start.y < end.y ? 1 : -1;
-  
-    let error = deltaX - deltaY;
-    const newPoints: Point[] = [];
-    while (currentX !== end.x || currentY !== end.y) {
-      newPoints.push({ x: currentX, y: currentY });
-  
-      const doubleError = error * 2;
-  
-      if (doubleError > -deltaY) {
-        error -= deltaY;
-        currentX += stepX;
-      }
-      if (doubleError < deltaX) {
-        error += deltaX;
-        currentY += stepY;
-      }
-    }
-    newPoints.push(end);
-    setPoints(newPoints);
-  };
-  
-
   return (
     <div className="Wrapper">
       <div className="MainArea">
@@ -125,30 +110,53 @@ const App: React.FC = () => {
               />
             ))
           ))}
-          {points.map((point, index) => (
-            <rect key={index} x={point.x * cellSize} y={point.y * cellSize} width={cellSize} height={cellSize} fill="red" />
+          {segmentEnds.map((point, index) => (
+            <rect 
+            key={index} 
+            x={point.x * cellSize} y={point.y * cellSize} 
+            width={cellSize} height={cellSize} 
+            fill={`rgba(${point.color.r}, ${point.color.g}, ${point.color.b}, ${point.opacity})`} 
+          />
+          ))}
+          {(debug ? points.slice(0, counter) : points).map((point, index) => (
+            <rect 
+            key={index} 
+            x={point.x * cellSize} y={point.y * cellSize} 
+            width={cellSize} height={cellSize} 
+            fill={`rgba(${point.color.r}, ${point.color.g}, ${point.color.b}, ${point.opacity})`} 
+          />
           ))}
         </svg>
         <div className="Menu">
-          <div>
-            <label>Show Grid: </label>
-            <input type="checkbox" checked={showGrid} onChange={toggleGrid} />
+          <div className="Input">
+            <label>Algorithm: </label>
+            <select value={selectedAlgorithm} onChange={handleAlgorithmChange}>
+              <option value="digitalDifferentialAnalyzer">Digital Differential Analyzer</option>
+              <option value="bresenhamAlgorithm">Bresenham Algorithm</option>
+              <option value="xiaolinWuAlgorithm">Wu Algorithm</option>
+            </select>
           </div>
-          <div>
+          <div className="Input">
             <label>Cell size: </label>
             <input type="text" value={cellSize} onChange={handleCellSizeChange} />
           </div>
-          <button onClick={digitalDifferentialAnalyzer}>Digital Differential Analyzer</button>
-          <button onClick={bresenhamAlgorithm}>Bresenham Algorithm</button>
+          <div className="debugSettings">
+            <div className="debugCheckbox">
+              <label>Show Grid: </label>
+              <input type="checkbox" checked={showGrid} onChange={toggleGrid} />
+            </div>
+            <div>
+              <label>Debug: </label>
+              <input type="checkbox" checked={debug} onChange={() => {setDebug(!debug)}} />
+            </div>
+          </div>
+          <div className="Buttons">
+            {!debugRunning && <button className="Run" onClick={handleRun}>Run</button>}
+            {debugRunning && <button className="Step" onClick={handleStep}>Next</button>}
+            <button className="Reset" onClick={handleReset}>Reset</button>
+          </div>
         </div>
       </div>
-      <ul className="PointList">
-        {points.map((point, index) => (
-          <li key={index}>
-            Point {index + 1}: ({point.x}, {point.y})
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
